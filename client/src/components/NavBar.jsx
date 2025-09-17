ad99ac70c02a1f483bb7b51093059a0f9f2a5233
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-09-17 15:51:55 +0800

    Added the setup and files

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className='flex justify-between m-5'>
+      <div>
+        <a href="" className='font-poppins font-medium text-3xl text-blue-700'>Blynk'n</a>
+      </div>
+        <div>
+          <a href="" className='hover:underline text-blue-700 rounded-lg py-2 px-3'> Settings</a>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { LuEyeClosed } from "react-icons/lu";
+import { FiEye } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <main className=''>
+        <section>
+           <div>
+            <LuEyeClosed className='text-blue-700 text-9xl' />
+              
+          </div> 
+        </section>
+    </main>
+  )
+}
+
+export default Home
