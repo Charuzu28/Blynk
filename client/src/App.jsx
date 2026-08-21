9dd932991285d94d4bc4f573433871faf9cd8974
Charles Alamares <146437969+Charuzu28@users.noreply.github.com>
2026-08-22 00:16:41 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,9 @@ import Home from './pages/Home';
 import NavBar from './components/NavBar';
 import Footer from "./components/Footer";
 import Settings from "./pages/Settings";
-import Maintenance from "./pages/Maintenance";
-
-const MAINTENANCE_MODE = true;
 
 function App() {
 
-  if (MAINTENANCE_MODE) {
-    return <Maintenance />;
-  }
-  
   return (
     <Router>
       <NavBar />
